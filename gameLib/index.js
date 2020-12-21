d11ec3f41f00dd1042161b2e63c69f8c6ea5dc31
Connor-OB <connorsuobrien@gmail.com>
2020-12-21 21:21:37 +0000

    * add makeBody method

--- gameLib/index.js
+++ gameLib/index.js
@@ -1,6 +1,41 @@
 (function(window, _) {
   window.myLibraryName = window.myLibraryName || {
     numz: {},
-    phyz: {},
+    phyz: {makeBody: function(type, {
+        velocityX = 0,
+        velocityY = 0,
+        rotationalVelocity = 0,
+        integrity = 1,
+        density = 1,
+        volatility = 0
+      } = {}) {
+        if (type === undefined) throw new Error('You must provide a valid String for the type parameter!');
+        return {
+          type: type,
+          velocityX: velocityX,
+          velocityY: velocityY,
+          rotationalVelocity: rotationalVelocity,
+          integrity: integrity,
+          density: density,
+          volatility: volatility,
+
+          /**
+           * @param {Number} A number representing the force of the impact.
+           * @param {Object} The other body involved in the collision.
+           */
+          handleCollision(impact, body) {
+            // template method //
+          },
+
+          /**
+           * Can be overridden in the concrete body to provide a custom update()
+           * method.
+           */
+          update(event) {
+            // template method //
+          }
+        };
+      },
+    },
   };
 }(window, window._));
